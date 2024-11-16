d9e4ae43b0b5b92879e4f00197b86954bca9c637
federmozzon <mozzonfederico@gmail.com>
2024-11-16 16:03:55 -0300

    nuevos cambios en el deploy

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/ocean_of_words',
+}
+
+module.exports = nextConfig
